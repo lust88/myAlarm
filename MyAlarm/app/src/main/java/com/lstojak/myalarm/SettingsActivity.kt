4ef24f4a4f7cae56de0b7763ea0b205d14b26c4c
lukaszstojaksm <lukasz.stojak@softwaremind.com>
2022-11-23 12:57:46 +0100

    main layout init

---
+++ MyAlarm/app/src/main/java/com/lstojak/myalarm/SettingsActivity.kt
@@ -0,0 +1,18 @@
+package com.lstojak.myalarm
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.lstojak.myalarm.databinding.ActivityMainBinding
+
+class SettingsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
